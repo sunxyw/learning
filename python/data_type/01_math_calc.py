92c778dea4002c10093692dd2935dc89a1912ec4
sunxyw <git@sunxyw.xyz>
2022-10-04 22:46:36 +0800

    python: math calculation

---
+++ python/data_type/01_math_calc.py
@@ -0,0 +1,26 @@
+"""
+Given a `math_op()` function, try the following math calculations and print the output:
+```
+(3 / 2)
+(3 // 2)
+(3 % 2)
+(3 ** 2)
+```
+Expected output:
+```
+1.5
+1
+1
+9
+```
+"""
+
+
+def math_op():
+    print(3 / 2)
+    print(3 // 2)
+    print(3 % 2)
+    print(3 ** 2)
+
+
+math_op()
