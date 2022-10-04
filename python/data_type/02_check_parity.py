ed4847b09e5e48e00a4ffe106727b6f1f31c930b
sunxyw <git@sunxyw.xyz>
2022-10-04 22:48:58 +0800

    python: check parity

---
+++ python/data_type/02_check_parity.py
@@ -0,0 +1,11 @@
+"""
+Determine if a given number is even or odd.
+Return 0 if the number is even, and 1 if it is odd.
+"""
+
+
+def check_parity(num):
+    return num % 2
+
+
+print(check_parity(3))
