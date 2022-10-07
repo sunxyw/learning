b3f4581ca9a8badf125b9a5d562316b8a0d601e8
sunxyw <git@sunxyw.xyz>
2022-10-07 19:02:42 +0800

    python(data_type-list): sublist

---
+++ python/data_type/list/01_sublist.py
@@ -0,0 +1,10 @@
+"""
+Given a `get_sublist()` function, accept a list of numbers and return the sublist of the first 3 elements.
+"""
+
+
+def get_sublist(nums):
+    return nums[:3]
+
+
+print(get_sublist([1, 2, 3, 4, 5]))
