a3509b7b2ea0a1f0a2805ea08331440d2b76a759
sunxyw <git@sunxyw.xyz>
2022-10-07 20:25:55 +0800

    python(module): find_gcd

---
+++ python/module/01_find_gcd.py
@@ -0,0 +1,13 @@
+"""
+Write a `find_gcd()` function that takes two numbers as arguments and returns the greatest common divisor of the
+two numbers.
+"""
+
+import math
+
+
+def find_gcd(a, b):
+    return math.gcd(a, b)
+
+
+print(find_gcd(12, 16))
