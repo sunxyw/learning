ff42051c91bb118e1ca537d170e7efca4fbb0ea2
sunxyw <git@sunxyw.xyz>
2022-10-08 16:52:03 +0800

    python(class): rectangle getter

---
+++ python/class/02_rectangle_getter.py
@@ -0,0 +1,12 @@
+class Rectangle:
+    def __init__(self, x1, y1, x2, y2):
+        self.x1 = x1
+        self.y1 = y1
+        self.x2 = x2
+        self.y2 = y2
+
+    def width(self):
+        return self.x2 - self.x1
+
+    def height(self):
+        return self.y2 - self.y1
